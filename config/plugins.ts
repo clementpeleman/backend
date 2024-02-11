714967524b5a8b505fe080b436e60514d05afd5b
clementpeleman <clementpeleman@outlook.com>
2024-02-11 14:02:00 +0100

    installed pg, aws upload and updated config files

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,22 @@
 // ./config/plugins.ts
 
 export default ({ env }) => ({
-  // ...
+  upload: {
+    config: {
+      provider: "aws-s3",
+      providerOptions: {
+        s3Options: {
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
+          params: {
+            Bucket: env("AWS_BUCKET_NAME"),
+          },
+        },
+      },
+    },
+  },
+
   "image-optimizer": {
     enabled: false,
     config: {
